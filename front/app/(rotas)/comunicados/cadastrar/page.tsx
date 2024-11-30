f7cc1e2bed34aaf38dfcb2aeeba44f18f17c4fca
MisterKingbad <davymichael2@gmail.com>
2024-11-30 19:44:50 -0300

    feat: :memo: Adicionando componentes estilizados.

--- front/app/(rotas)/comunicados/cadastrar/page.tsx
+++ front/app/(rotas)/comunicados/cadastrar/page.tsx
@@ -6,14 +6,47 @@ import {
   Card,
   Container,
   FormControl,
+  FormControlLabel,
   Grid2,
   InputLabel,
   MenuItem,
   Select,
+  styled,
   Switch,
   TextField,
   Typography,
 } from "@mui/material";
+import FileUploadIcon from '@mui/icons-material/FileUpload';
+import BackupIcon from '@mui/icons-material/Backup';
+import LinkIcon from '@mui/icons-material/Link';
+
+const Android12Switch = styled(Switch)(({ theme }) => ({
+  '& .MuiSwitch-track': {
+    borderRadius: 22 / 2,
+    '&::before, &::after': {
+      content: '""',
+      position: 'absolute',
+      top: '50%',
+      transform: 'translateY(-50%)',
+      width: 16,
+      height: 16,
+    },
+  },
+'& .MuiSwitch-switchBase': {
+    transitionDuration: '300ms',
+    '&.Mui-checked': {
+      transform: 'translateX(16px)',
+      color: '#273481',
+      '& + .MuiSwitch-track': {
+        backgroundColor: '#273481',
+        opacity: 0.5,
+      },
+      '&.Mui-disabled + .MuiSwitch-track': {
+        opacity: 0.5,
+      },
+    },
+  },
+}));
 
 export default function CadastrarComunicado() {
   return (
@@ -119,6 +152,8 @@ export default function CadastrarComunicado() {
                   padding: "8px 16px",
                   fontSize: "14px",
                 }}
+                endIcon={<LinkIcon/>}
+                disabled
               >
                 Copiar Link
               </Button>
@@ -154,7 +189,7 @@ export default function CadastrarComunicado() {
                 Conteúdo irá gerar POP-UP?
               </Typography>
               <Box display="flex" alignItems={"center"} gap={1}>
-                <Switch defaultChecked />
+                <Android12Switch defaultChecked />
                 <Typography fontWeight="bold">Sim</Typography>
               </Box>
             </Grid2>
@@ -172,17 +207,26 @@ export default function CadastrarComunicado() {
             </Grid2>
             <Grid2 size={20}>
               <Box display="flex" gap={3}>
-                <Button variant="contained" color="inherit" sx={{
-                  backgroundColor: "#642683",
+                <Button startIcon={<FileUploadIcon/>} variant="contained" color="inherit" sx={{
+                  backgroundColor: "#ffffff",
                   "&:hover": { backgroundColor: "#7f3da0" },
                 }}>
                   Insira os arquivos do comunicado.
                 </Button>
-                <Button variant="contained" color="secondary" sx={{
+                <Button endIcon={<BackupIcon/>} variant="contained" color="secondary" sx={{
                   backgroundColor: "#642683",
                   "&:hover": { backgroundColor: "#7f3da0" },
+                  border: 1, borderStyle: "dashed", borderColor: "black"
                 }}>
                   Insira imagem para o card
+                </Button>
+              </Box>
+            </Grid2>
+            <Grid2 size={20}>
+              <Box display="flex" justifyContent={"space-between"}>
+                <Box></Box>
+                <Button variant="contained" color="primary">
+                  Enviar Comunicado
                 </Button>
               </Box>
             </Grid2>

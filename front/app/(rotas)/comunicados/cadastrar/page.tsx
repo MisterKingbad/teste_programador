ac391f176590a3920496c95459b6975be48b77f5
MisterKingbad <davymichael2@gmail.com>
2024-11-30 19:07:41 -0300

    feat: :memo: Add formulario

--- front/app/(rotas)/comunicados/cadastrar/page.tsx
+++ front/app/(rotas)/comunicados/cadastrar/page.tsx
@@ -9,23 +9,11 @@ import {
   Grid2,
   InputLabel,
   MenuItem,
-  Paper,
   Select,
-  styled,
+  Switch,
   TextField,
   Typography,
 } from "@mui/material";
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-  ...theme.applyStyles("dark", {
-    backgroundColor: "#1A2027",
-  }),
-}));
 
 export default function CadastrarComunicado() {
   return (
@@ -40,15 +28,18 @@ export default function CadastrarComunicado() {
           alignItems="center"
           pt={4}
           pb={1}
-          px={9}
+          px={3}
         >
           <Typography fontWeight="bold">Criação de comunicado</Typography>
           <Grid2 container spacing={2}>
+            <Typography fontWeight="bold" variant="subtitle2" mt={2}>
+              Operações
+            </Typography>
             <Grid2 size={20}>
               <FormControl size="small" fullWidth id="input-nome">
-                <InputLabel id="demo-select-small-label">
-                  Selecione o tipo de conteúdo
-                </InputLabel>
+                <Typography variant="subtitle2" mb={1}>
+                  Selecione operações para carregar as respectivas carteiras
+                </Typography>
                 <Select
                   fullWidth
                   size="small"
@@ -60,10 +51,25 @@ export default function CadastrarComunicado() {
                 </Select>
               </FormControl>
             </Grid2>
+            <Typography fontWeight="bold" variant="subtitle2" mt={2}>
+              Detalhes do comunicado
+            </Typography>
             <Grid2 size={20}>
               <FormControl size="small" fullWidth id="input-nome">
+                <TextField
+                  id="standard-basic"
+                  label="Insira um titulo para o comunicado"
+                  variant="standard"
+                />
+              </FormControl>
+            </Grid2>
+            <Grid2 size={20} mb={1}>
+              <Typography fontWeight="bold" variant="subtitle2" >
+                Tipo de Usuário:
+              </Typography>
+              <FormControl size="small" fullWidth id="input-nome">
                 <InputLabel id="demo-select-small-label">
-                  Selecione o tipo de conteúdo
+                  Selecione os tipos de usuário
                 </InputLabel>
                 <Select
                   fullWidth
@@ -77,15 +83,23 @@ export default function CadastrarComunicado() {
               </FormControl>
             </Grid2>
             <Grid2>
+              <Typography fontWeight="bold" variant="subtitle2" mt={-2.5}>
+                Selecione a Procedimento
+              </Typography>
               <FormControl
                 size="small"
                 sx={{
-                    minWidth: { xs: "158px", sm: "289px", md: "593px", lg: "1578px" },
-                    maxWidth: "100%", // Garante que não ultrapasse os limites
-                  }}
+                  minWidth: {
+                    xs: "158px",
+                    sm: "289px",
+                    md: "593px",
+                    lg: "1578px",
+                  },
+                  maxWidth: "100%",
+                }}
               >
                 <InputLabel id="demo-select-small-label">
-                  Selecione o tipo de conteúdo
+                  Select...
                 </InputLabel>
                 <Select label="Selecione o tipo de conteúdo">
                   <MenuItem value="">
@@ -110,26 +124,67 @@ export default function CadastrarComunicado() {
               </Button>
             </Grid2>
             <Grid2 size={20}>
+              <Typography fontWeight="bold" variant="subtitle2">
+                Conteúdo do comunicado
+              </Typography>
               <FormControl size="small" fullWidth id="input-nome">
                 <TextField
-                    id="outlined-multiline-static"
-                    label="Multiline"
-                    multiline
-                    rows={4}
-                    defaultValue="Default Value"
+                  id="outlined-multiline-static"
+                  multiline
+                  rows={4}
+                  defaultValue="Default Value"
                 />
               </FormControl>
             </Grid2>
             <Grid2 size={20}>
+              <Typography fontWeight="bold" variant="subtitle2">
+                Breve descrição
+              </Typography>
               <FormControl size="small" fullWidth id="input-nome">
                 <TextField
-                    id="outlined-multiline-static"
-                    label="Multiline"
-                    multiline
-                    rows={4}
-                    defaultValue="Default Value"
+                  id="outlined-multiline-static"
+                  multiline
+                  rows={4}
+                  defaultValue="Default Value"
                 />
               </FormControl>
+            </Grid2>
+            <Grid2 size={20}>
+              <Typography fontWeight="bold" variant="subtitle2">
+                Conteúdo irá gerar POP-UP?
+              </Typography>
+              <Box display="flex" alignItems={"center"} gap={1}>
+                <Switch defaultChecked />
+                <Typography fontWeight="bold">Sim</Typography>
+              </Box>
+            </Grid2>
+            <Typography fontWeight="bold" variant="subtitle2">
+              Escolha a data limite para a leitura do POP-UP
+            </Typography>
+            <Grid2 size={20} sx={{ '& > :not(style)': { width: '25ch' } }}>
+              <FormControl size="small" fullWidth id="input-nome">
+                <TextField
+                  id="standard-basic"
+                  label="Data limite para leitura"
+                  variant="standard"
+                />
+              </FormControl>
+            </Grid2>
+            <Grid2 size={20}>
+              <Box display="flex" gap={3}>
+                <Button variant="contained" color="inherit" sx={{
+                  backgroundColor: "#642683",
+                  "&:hover": { backgroundColor: "#7f3da0" },
+                }}>
+                  Insira os arquivos do comunicado.
+                </Button>
+                <Button variant="contained" color="secondary" sx={{
+                  backgroundColor: "#642683",
+                  "&:hover": { backgroundColor: "#7f3da0" },
+                }}>
+                  Insira imagem para o card
+                </Button>
+              </Box>
             </Grid2>
           </Grid2>
         </Box>
